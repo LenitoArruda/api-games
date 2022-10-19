e41fd6d768b8a0b1273803405715a16416f852cb
LenitoArruda <lenitoarruda@hotmail.com>
2022-10-19 14:30:37 -0300

    Axios

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(cors());
 
 
 var DB = {
@@ -55,6 +57,7 @@ app.get("/game/:id", (req,res) => {
             res.sendStatus(404);
         }
     }
+    
     
 });
 
